f05618a4f0224f47a2d6df5cc6af7e9709b3b597
Tomo Masakura <tomo.masakura@gmail.com>
2014-12-14 18:08:41 +0900

    pageObject をちょっとだけ良くした

--- app/scripts/pageObject.js
+++ app/scripts/pageObject.js
@@ -1,17 +1,40 @@
-var PageObject = (function (_) { // jshint ignore:line
+var PageObject = (function (_, $) { // jshint ignore:line
   'use strict';
 
-    var PageObject = function ($el) {
-    this.$el = $el || $(document);
+  var PageObject = function (options) {
+    var el;
+    if (options instanceof PageObject.$ || options instanceof HTMLElement) {
+      el = options;
+    } else if (options && options.el) {
+      el = options.el;
+    } else if (this.el) {
+      el = this.el;
+    } else {
+      el = document;
+    }
+
+    if (_.isFunction(el)) {
+      el = el.call(this, options);
+    } else if (el instanceof PageObject.$) {
+      el = el[0];
+    }
+
+    this.el = el;
+    this.$el = $(el);
+    this.parent = options ? options.parent : undefined;
   };
 
   _.extend(PageObject.prototype, {
     $: function () {
       return this.$el.find.apply(this.$el, arguments);
+    },
+    click: function (selector) {
+      this.$(selector).trigger('click');
     }
   });
 
   _.extend(PageObject, {
+    $: $,
     extend: function (properties, optClassProperties) {
       var parent = this;
       var child = function () { return parent.apply(this, arguments); };
@@ -24,4 +47,4 @@ var PageObject = (function (_) { // jshint ignore:line
   });
 
   return PageObject;
-})(_);
+})(_, $);
